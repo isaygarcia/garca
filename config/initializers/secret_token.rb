bacce6a4f5b7d5ea6925f3212fa975fef3a1a99d
Isay Garcia <isay@solcom-msi.(none)>
2013-03-12 10:41:03 -0600

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garca::Application.config.secret_token = 'b1320105acbaee33b7f9da244f8c53f2617aee9ec0d15f78f1fe41b07cadb4b3b8d3c76531d792904a5dc72499e63910f8a8630b2f66153728f1b858f3f4ef23'
